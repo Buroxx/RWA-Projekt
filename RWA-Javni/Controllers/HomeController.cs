137900865198cfd32bb3b9d298e9543c25504fe1
Buroxx <frangregec2110@gmail.com>
2022-06-21 21:08:40 +0200

    Pocetna zapoceta

--- RWA-Javni/Controllers/HomeController.cs
+++ RWA-Javni/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RWA_Javni.Models.DBRepo;
+using rwaLib.Dal;
+using rwaLib.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +14,22 @@ namespace RWA_Javni.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            //string path = Request.PhysicalApplicationPath.ToString();
+            //string imagePath = path.Remove(path.Length - 10);
+            //imagePath = imagePath + "RWA-Projekt"
+            //ViewBag.Path = imagePath;
+
+            IList<City> cities = new List<City>();
+            cities = DBCityManager.GetAllCities();
+
+            List<SelectListItem> selectList = new List<SelectListItem>();
+            foreach (City city in cities)
+            {
+                selectList.Add(new SelectListItem() { Text = city.Name, Value = city.Id.ToString()});
+            }
+
+            ViewBag.itemsToPick = selectList;
+
             return View();
         }
     }
---
+++ RWA-Javni/Models/DBRepo/DBCityManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ApplicationBlocks.Data;
+using rwaLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace RWA_Javni.Models.DBRepo
+{
+    public class DBCityManager
+    {
+        private static string CS = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+
+        public static IList<City> GetAllCities()
+        {
+            IList<City> cities = new List<City>();
+
+            var tblCities = SqlHelper.ExecuteDataset(CS, nameof(GetAllCities)).Tables[0];
+            if (tblCities == null) return null;
+
+            foreach (DataRow row in tblCities.Rows)
+            {
+                cities.Add(new City
+                {
+                    Id = (int)row[nameof(City.Id)],
+                    Name = row[nameof(City.Name)].ToString(),
+                });
+            }
+            return cities;
+        }
+    }
+}
